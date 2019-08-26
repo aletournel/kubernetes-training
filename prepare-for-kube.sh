7e01850682914b1898dc9624172e57b64e6cfb1e
Adrien LETOURNEL <aletournel@talend.com>
2019-08-26 14:14:26 +0200

    fix: coredns crashloop back off due to sellinux

--- prepare-for-kube.sh
+++ prepare-for-kube.sh
@@ -6,7 +6,7 @@ sed -i 's|^/swapfile|#/swapfile|' /etc/fstab
 
 # Disable SELinux
 setenforce 0
-sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+sed -i 's/^SELINUX=enforcing$/SELINUX=disabled/' /etc/selinux/config
 
 # Handle Yum
 
